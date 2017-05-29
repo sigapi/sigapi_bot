ee66cd56edeab95dd35d738d08036c8812b16daf
Marco Antonio Rocha <marco.rocha@matera.com>
2017-05-29 00:23:44 -0300

    Removendo controllers não utilizados e configurando o monitoramento da fila no scheduler

--- readme.md
+++ readme.md
@@ -6,3 +6,10 @@
 # Anotações gerais
 
 docker run --rm --user $(id -u):$(id -g) -v $(pwd):/app -v composer/composer create-project --prefer-dist laravel/laravel sigapi-bot
+
+conectar - Realiza a conexão ao serviço
+desconectar - Desfaz a conexão ao serviço
+dados_cadastrais - Obtém os dados cadastrais
+dados_desempenho - Obtém os dados de desempenho
+notas_parciais - Obtém as notas parciais
+faltas_parciais - Obteḿ as faltas parciais
--- sigapi-bot/app/Console/Kernel.php
+++ sigapi-bot/app/Console/Kernel.php
@@ -5,16 +5,46 @@ namespace App\Console;
 use App\Services\Bot\GetUpdatesService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel {
 
     protected function schedule(Schedule $schedule) {
 
         $schedule->call(function() {
+            $this->monitorQueue();
+        })->name('monitor_queue_listener')->everyMinute();
+
+        $schedule->call(function() {
             self::repeatAfterSeconds(function() {
                 (new GetUpdatesService())->getUpdates();
             }, 5);
         })->everyMinute();
+
+    }
+
+    // https://gist.github.com/mauris/11375869#gistcomment-1818901
+    protected function monitorQueue() {
+
+        $run_command = false;
+        $monitor_file_path = storage_path('queue.pid');
+
+        if (file_exists($monitor_file_path)) {
+            $pid = file_get_contents($monitor_file_path);
+            $result = exec("ps -p $pid --no-heading | awk '{print $1}'");
+
+            if ($result == '') {
+                $run_command = true;
+            }
+        } else {
+            $run_command = true;
+        }
+
+        if ($run_command) {
+            $command = 'php '. base_path('artisan'). ' queue:listen > /dev/null & echo $!';
+            $number = exec($command);
+            file_put_contents($monitor_file_path, $number);
+        }
 
     }
 
